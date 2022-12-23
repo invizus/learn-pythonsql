4bb1d0a5bafbfaba283d05614210ab7ee4bb9504
invizus <invizus@gmail.com>
2022-12-23 20:53:18 +0000

    finish of first leson

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
-class Employee:
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+engine = create_engine("mysql+mysqlconnector://roman:passw0rd@localhost/clockin")
+
+class Employee(Base):
+    __tablename__ = "employee"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(60), nullable=False)
+    department = Column(String(20))
+
     def __init__(self, name, department):
         self.name = name
         self.department = department
