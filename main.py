3e28203c4ea43586060d1a09c2e10f700391d844
invizus <invizus@gmail.com>
2022-12-25 18:42:52 +0000

    orm add records into db

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
-
-engine = create_engine("mysql+mysqlconnector://roman:passw0rd@localhost/clockin")
 
 class Employee(Base):
     __tablename__ = "employee"
@@ -21,6 +20,16 @@ class Employee(Base):
     def info(self):
         return f"{self.name} {self.department}"
 
-Messi = Employee(department="manager", name="Messi")
-#Messi = Employee("Messi", "manager")
-print(Messi.info())
+#Messi = Employee(department="manager", name="Messi")
+##Messi = Employee("Messi", "manager")
+#print(Messi.info())
+
+engine = create_engine("mysql+mysqlconnector://roman:passw0rd@localhost/clockin")
+Base.metadata.create_all(engine)
+
+u1 = Employee(name="Roman", department="IT")
+#print(u1(info))
+Session = sessionmaker(bind=engine)
+session = Session()
+session.add(u1)
+session.commit()
